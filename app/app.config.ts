a1b40890f1d3368803f4dbb5b7c3a6e7f0cd16fd
iicemeta <richardmiku@vip.qq.com>
2026-03-28 16:00:23 +0800

    refactor: 移除旧内容并更新博客配置
    更新博客配置，包括标题、作者信息和网站URL。删除大量旧文章和资源文件，涉及技术、生活、安全等多个类别。移除的内容包括个人随笔、技术教程、安全分析等。同时更新favicon和头像路径，调整版权信息。此次变更为博客内容重构的一部分，旨在精简和更新网站内容。

--- app/app.config.ts
+++ app/app.config.ts
@@ -93,11 +93,11 @@ export default defineAppConfig({
 
 	/** 左侧栏顶部 Logo */
 	header: {
-		logo: 'https://weavatar.com/avatar/47c0f2e82b76d9b10eb3023df9e02e4e3fdbeaf5b74b842063f207971e7fbe7b?s=160',
+		logo: '/images/avatar.webp',
 		/** 展示标题文本，否则展示纯 Logo */
 		showTitle: true,
 		subtitle: blogConfig.subtitle,
-		emojiTail: ['📄', '🦌', '🙌', '🐟', '🏖️'],
+		emojiTail: [],
 	},
 
 	/** 友链页面 */
--- blog.config.ts
+++ blog.config.ts
@@ -1,26 +1,26 @@
 import type { FeedEntry } from './app/types/feed'
 
 const basicConfig = {
-	title: '纸鹿摸鱼处',
-	subtitle: '纸鹿至麓不知路，支炉制露不止漉',
+	title: 'ice-cream',
+	subtitle: '如奶油般化开',
 	// 长 description 利好于 SEO
-	description: '纸鹿本鹿的个人博客，分享技术与生活。“折腾不止，摸鱼生活——摸门🙏🏻”。纸鹿是一名开源爱好者，结识了许多志同道合的朋友。这个博客记录了他在生活和技术学习中的点滴经历，充满启发与思考。网站界面简洁美观，内容丰富实用，人气互动活跃，涵盖了编程、生活、学习等多个领域，为读者提供了卓越的阅读体验。',
+	description: 'iice个人网站，分享技术，分享生活。',
 	author: {
-		name: '纸鹿本鹿',
-		avatar: 'https://www.zhilu.site/api/avatar.png',
-		email: 'hi@zhilu.cyou',
-		homepage: 'https://www.zhilu.site/',
+		name: 'iicemeta',
+		avatar: '/images/avatar.webp',
+		email: 'me@iicemeta.com',
+		homepage: 'https://www.iicemeta.com',
 	},
 	copyright: {
 		abbr: 'CC BY-NC-SA 4.0',
 		name: '署名-非商业性使用-相同方式共享 4.0 国际',
 		url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh-hans',
 	},
-	favicon: 'https://www.zhilu.site/api/icon.png',
+	favicon: '/images/favicon.svg',
 	language: 'zh-CN',
-	timeEstablished: '2019-07-19',
+	timeEstablished: '2026-03-28',
 	timeZone: 'Asia/Shanghai',
-	url: 'https://blog.zhilu.site/',
+	url: 'https://www.iicemeta.com',
 	defaultCategory: '未分类',
 }
 
